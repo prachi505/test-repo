e84142415b7b6d00dd8fe9ed9efc744dbb321da0
prachi505 <79562803+prachi505@users.noreply.github.com>
2021-03-19 13:22:52 +0530

    Add files via upload

---
+++ QUE1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+int main() 
+{
+	int n,i,j,temp;
+	j=-1;
+	printf("enter the size of array");
+	scanf("%d",&n);
+	int a[n];
+	printf("enter the elemts of array \n");
+	for(i=0;i<n;i++)
+	{
+	    scanf("%d",&a[i]);
+	}
+	for(i=0;i<n;i++)
+	{
+		if(a[i]%2==0)
+		{
+			j++;
+			temp=a[i];
+			a[i]=a[j];
+			a[j]=temp;
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d \n",a[i]);
+	}
+	return 0;
+}
+	
+	
